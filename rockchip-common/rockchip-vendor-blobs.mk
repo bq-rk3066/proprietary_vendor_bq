e23d7032451af42852238d78ee69182b9a9f12f2
Kra1o5 <kra1o5x@gmail.com>
2014-07-11 10:16:29 +0200

    Add Widevine blobs

--- rockchip-common/rockchip-vendor-blobs.mk
+++ rockchip-common/rockchip-vendor-blobs.mk
@@ -58,6 +58,15 @@ PRODUCT_COPY_FILES += \
     vendor/bq/rockchip-common/proprietary/lib/hw/gpu.rk30board.so:system/lib/hw/gpu.rk30board.so \
     vendor/bq/rockchip-common/proprietary/lib/hw/gralloc.rk30board.so:system/lib/hw/gralloc.rk30board.so \
     vendor/bq/rockchip-common/proprietary/lib/hw/hwcomposer.rk30board.so:system/lib/hw/hwcomposer.rk30board.so
+    
+# Widevine
+PRODUCT_COPY_FILES += \
+    vendor/bq/rockchip-common/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
+    vendor/bq/rockchip-common/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
+    vendor/bq/rockchip-common/proprietary/vendor/lib/libdrmdecrypt.so:system/vendor/lib/libdrmdecrypt.so \
+    vendor/bq/rockchip-common/proprietary/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
+    vendor/bq/rockchip-common/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
+    vendor/bq/rockchip-common/proprietary/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so
 
 # Firmware
 PRODUCT_COPY_FILES += \
